9c37c2d91c4ff5b4e0c96ae96e676e9151aead18
Brian <hassankbrian@gmail.com>
2013-05-29 22:59:21 -0430

    Finished enabling profile edition

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,11 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
   def new
     @user = User.new
   end
+
   def create
     @user = User.new(params[:user])
     if @user.save
@@ -14,6 +16,21 @@ class UsersController < ApplicationController
       # Handle a successful save.
     else
       render 'new'
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+   def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated"
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
     end
   end
 end
