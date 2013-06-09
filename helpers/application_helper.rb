e1f769a61ab996811fe22151b937a001212fe75d
Brian <hassankbrian@gmail.com>
2013-06-09 13:13:24 -0430

    checking commit

--- helpers/application_helper.rb
+++ helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
 	#returns the full title of a page
 	def full_title(page_title)
-		base_title="Ruby on Rails Tutorial Sample App"
+		base_title="Ruby on Rails Sample App"
 		if page_title.empty?
 			base_title
 		else
